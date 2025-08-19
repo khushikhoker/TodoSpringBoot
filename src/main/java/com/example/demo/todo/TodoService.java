e9e70c4775e41d3eda111d2591ead6f61ec5c2b0
Khushi <khushikhoker9@gmail.com>
2025-08-19 19:10:03 +0530

    Add add todo feature

--- src/main/java/com/example/demo/todo/TodoService.java
+++ src/main/java/com/example/demo/todo/TodoService.java
@@ -10,14 +10,23 @@ import java.util.List;
 public class TodoService {
 
     private static List<Todo> todoList = new ArrayList<>();
+
+    private static int todosCount =0;
+
+
     static {
-        todoList.add(new Todo(1, "in28minutes", "Learn Aws", LocalDate.now().plusYears(1),false));
-        todoList.add(new Todo(2, "in28minutes", "Learn devops", LocalDate.now().plusYears(2),false));
-        todoList.add(new Todo(3, "in28minutes", "Learn full stack", LocalDate.now().plusYears(3),false));
+        todoList.add(new Todo(++todosCount, "in28minutes", "Learn Aws", LocalDate.now().plusYears(1),false));
+        todoList.add(new Todo(++todosCount, "in28minutes", "Learn devops", LocalDate.now().plusYears(2),false));
+        todoList.add(new Todo(++todosCount, "in28minutes", "Learn full stack", LocalDate.now().plusYears(3),false));
 
     }
 
     public List<Todo> findByUserName(String username) {
         return todoList;
+    }
+
+    public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+        Todo todo = new Todo(++todosCount, username, description, targetDate,done);
+        todoList.add(todo);
     }
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
@@ -156,6 +156,7 @@
                     </c:forEach>
                 </tbody>
             </table>
+            <a href="add-todo" class="btn btn-success">Add Todo</a>
         </div>
     </div>
 
