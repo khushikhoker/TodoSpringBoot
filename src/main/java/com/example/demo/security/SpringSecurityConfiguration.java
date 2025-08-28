b76dbdce3fd700495ffa2c89ad35127055262412
Khushi <khushikhoker9@gmail.com>
2025-08-29 01:03:37 +0530

    add h2 in memory database setup

--- src/main/java/com/example/demo/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/demo/security/SpringSecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.example.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.net.PasswordAuthentication;
 import java.util.function.Function;
@@ -32,5 +35,21 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    //All URLs are protected
+    //A login form is shown for unauthorizes result
+
+    //CSRF disable
+    //Frames
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        return http.build();
     }
 }
